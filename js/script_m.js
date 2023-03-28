991ddaf9f53ee890a36368ba9184704a3fbe3339
张鬼鬼 <72719472+xiangyangsvip@users.noreply.github.com>
2023-03-29 08:52:11 +0900

    Add files via upload
    携帯のwebナビゲーションバーが未完成です。 pc版のwebナビゲーションバーのドットが未完成です。

--- js/script_m.js
+++ js/script_m.js
@@ -1,21 +1,17 @@
 window.onload = function () {
-    alert('test');
     $("a").click(function () {
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top - 20 + "px" }, 500);
     });
-
     window.addEventListener("scroll", headerfixd);
     function headerfixd() {
         if ($(window).scrollTop() >= 5) {
-            alert('test');
             $('#m_header').css('background', '#0780A3');
-            $('#m_header #m_icon').css('background', 'url(src/png/m/menu_m_w.png)');
+            $('#m_header #m_icon').css('background', 'url(src/png/m/menu_m_w.png) no-repeat center');
         } else {
             $('#m_header').css('background', 'rgba(0, 0, 0, 0)');
-            $('#m_header #m_icon').css('background', 'url(src/png/m/menu_m.png)');
+            $('#m_header #m_icon').css('background', 'url(src/png/m/menu_m.png) no-repeat center');
         }
     }
-
 }
 
 
