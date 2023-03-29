c08fae129a0deeeef507e99981855be22687e32e
张鬼鬼 <72719472+xiangyangsvip@users.noreply.github.com>
2023-03-29 09:59:21 +0900

    20230329
    pc版ナビゲーションバーにバグがあります

--- js/script_m.js
+++ js/script_m.js
@@ -1,7 +1,5 @@
 window.onload = function () {
-    $("a").click(function () {
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top - 20 + "px" }, 500);
-    });
+
     window.addEventListener("scroll", headerfixd);
     function headerfixd() {
         if ($(window).scrollTop() >= 5) {
@@ -12,6 +10,28 @@ window.onload = function () {
             $('#m_header #m_icon').css('background', 'url(src/png/m/menu_m.png) no-repeat center');
         }
     }
+    $('#m_icon').click(function () {
+        if ($('#m_menu').css('display') == 'block') {
+            $('#m_menu').css('display', 'none');
+            $('#m_icon').css('background', 'url(src/png/m/menu_m_w.png) no-repeat center');
+            if ($(window).scrollTop() < 5) {
+                $('#m_header').css('background', 'rgba(0, 0, 0, 0)');
+                $('#m_icon').css('background', 'url(src/png/m/menu_m.png) no-repeat center');
+            }
+        } else {
+        $('#m_menu').css('display', 'block');
+        $('#m_icon').css('background', 'url(src/png/m/close_m.png) no-repeat center');
+        if ($(window).scrollTop() < 5) {
+            $('#m_header').css('background', '#0780A3');
+        }
+        }
+    }); 
+    $('#m_menu  a').click(function () {
+        if ($('#m_menu').css('display') == 'block') {
+            $('#m_menu').css('display', 'none');
+            }
+        } 
+    );
 }
 
 
