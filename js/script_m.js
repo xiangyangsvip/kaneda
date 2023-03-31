6dfb55c1dd273b4f0aad8b557b4f51a04d92cd12
张鬼鬼 <72719472+xiangyangsvip@users.noreply.github.com>
2023-03-31 18:04:16 +0900

    金田さんのpcと携帯のページを修正しました。
    金田さんのpcと携帯のページを修正しました。

--- js/script_m.js
+++ js/script_m.js
@@ -11,24 +11,32 @@ window.onload = function () {
         }
     }
     $('#m_icon').click(function () {
-        if ($('#m_menu').css('display') == 'block') {
+        if ($('#m_menu').css('display') == 'none') {
+            window.removeEventListener("scroll", headerfixd);
+            $('#m_menu').css('display', 'block');
+            $('#m_icon').css('background', 'url(src/png/m/close_m.png) no-repeat center');
+            if ($(window).scrollTop() < 5) {
+                $('#m_header').css('background', '#0780A3');
+            }
+
+        }
+        else {
+            window.addEventListener("scroll", headerfixd);
             $('#m_menu').css('display', 'none');
             $('#m_icon').css('background', 'url(src/png/m/menu_m_w.png) no-repeat center');
             if ($(window).scrollTop() < 5) {
                 $('#m_header').css('background', 'rgba(0, 0, 0, 0)');
                 $('#m_icon').css('background', 'url(src/png/m/menu_m.png) no-repeat center');
             }
-        } else {
-        $('#m_menu').css('display', 'block');
-        $('#m_icon').css('background', 'url(src/png/m/close_m.png) no-repeat center');
-        if ($(window).scrollTop() < 5) {
-            $('#m_header').css('background', '#0780A3');
         }
-        }
-    }); 
+
+    });
+
     $('#m_menu  a').click(function () {
         if ($('#m_menu').css('display') == 'block') {
             $('#m_menu').css('display', 'none');
+            window.addEventListener("scroll", headerfixd);
+
             }
         } 
     );
